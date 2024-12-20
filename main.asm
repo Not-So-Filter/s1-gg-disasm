32a15ae9a779336eb5afcc7c25964a64dcd4d94f
Filter <not.so.filter@hotmail.com>
2024-12-19 20:08:08 -0500

    fix building (thanks naoto :))

--- main.asm
+++ main.asm
@@ -9,9 +9,9 @@ START:
 	IM	1
 	
 _LOOP:
-	IN	A,	($7E)
+	IN	A,($7E)
 	CP	$B0
-	JR	NZ,	_LOOP
+	JR	NZ,_LOOP
 	JP	$0296
 	
 	ALIGN	$18
@@ -37,9 +37,9 @@ VINT:
 	
 	DI
 	PUSH	AF
-	LD	A,	(IY+$07)
+	LD	A,(IY+$07)
 	XOR	$08
-	LD	(IY+$07),	A
+	LD	(IY+$07),A
 	POP	AF
 	EI
 	RET
@@ -50,51 +50,51 @@ VINT_JUMP:
 	PUSH	HL
 	PUSH	DE
 	PUSH	BC
-	IN	A,	($BF)
-	BIT	7,	(IY+$06)
-	JR	Z,	LOC_AC
-	LD	A,	($D241)
+	IN	A,($BF)
+	BIT	7,(IY+$06)
+	JR	Z,LOC_AC
+	LD	A,($D241)
 	AND	A
-	JP	NZ,	$01B4
-	LD	A,	($D2DC)
+	JP	NZ,$01B4
+	LD	A,($D2DC)
 	AND	A
-	JR	Z,	LOC_AC
+	JR	Z,LOC_AC
 	CP	$FF
-	JR	Z,	LOC_AC
-	LD	($D242),	A
-	LD	A,	$0A
-	OUT	($BF),	A
-	LD	A,	$8A
-	OUT	($BF),	A
-	LD	A,	($D219)
+	JR	Z,LOC_AC
+	LD	($D242),A
+	LD	A,$0A
+	OUT	($BF),A
+	LD	A,$8A
+	OUT	($BF),A
+	LD	A,($D219)
 	OR	$10
-	OUT	($BF),	A
-	LD	A,	$80
-	OUT	($BF),	A
-	LD	A,	3
-	LD	($D241),	A
+	OUT	($BF),A
+	LD	A,$80
+	OUT	($BF),A
+	LD	A,3
+	LD	($D241),A
 	
 LOC_AC:
 	PUSH	IX
 	PUSH	IY
-	LD	HL,	($D22F)
+	LD	HL,($D22F)
 	PUSH	HL
-	BIT	0,	(IY+$00)
-	CALL	NZ,	$0180
-	BIT	0,	(IY+$00)
-	CALL	Z,	$00F0
+	BIT	0,(IY+$00)
+	CALL	NZ,$0180
+	BIT	0,(IY+$00)
+	CALL	Z,$00F0
 	EI
-	LD	A,	3
-	LD	($FFFE),	A
-	LD	($D22F),	A
+	LD	A,3
+	LD	($FFFE),A
+	LD	($D22F),A
 	CALL	$4000
 	CALL	$0602
-	BIT	4,	(IY+3)
-	CALL	Z,	$00EB
+	BIT	4,(IY+3)
+	CALL	Z,$00EB
 	CALL	$0688
 	POP	HL
-	LD	($FFFE),	HL
-	LD	($D22F),	HL
+	LD	($FFFE),HL
+	LD	($D22F),HL
 	POP	IY
 	POP	IX
 	POP	BC
--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
 @echo off
+if exist s1built.gg move /y s1built.gg s1built.prev.gg >NUL
 "tool/vasmz80_psi-x.exe" -maxerrors=0 -Fbin -start=0 -o s1built.gg -L main.lst -Lall main.asm 2> errors.log
 pause
