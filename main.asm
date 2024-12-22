3534a2dc11c12cf6aba7d4f65fd74f1929c72be5
Filter <not.so.filter@hotmail.com>
2024-12-22 13:51:20 -0500

    palettes split

--- main.asm
+++ main.asm
@@ -33,7 +33,7 @@ VINT:
 	JP	VINT_JUMP
 	
 	; Header information, removed in the international release
-	DB	"Developed By (C) 1991 Ancient - S", $A5, "Hayashi."
+	DB	"Developed By (C) 1991 Ancient - S",$A5,"Hayashi."
 	DB	0
 	
 	; Uncertain
@@ -160,3 +160,120 @@ SUB_F0:
 	OUT	($BF),A
 	SET	0,(IY+0)
 	RET
+	
+SUB_172:
+	BIT	7,(IY+6)
+	JR	NZ,LOC_17C
+	
+LOC_178:
+	CALL	$586
+	RET
+	
+LOC_17C:
+	CALL	SUB_185
+	RET
+	
+SUB_180:
+	BIT	7,(IY+6)
+	RET	Z
+	
+SUB_185:
+	LD	A,($D2DC)
+	AND	A
+	JR	Z,LOC_178
+	CP	$FF
+	JR	NZ,LOC_178
+	LD	HL,DATA_216
+	BIT	4,(IY+7)
+	JR	Z,LOC_19B
+	LD	HL,DATA_256
+	
+LOC_19B:
+	CALL	SUB_19F
+	RET
+	
+SUB_19F:
+	LD	B,$20
+	LD	A,0
+	OUT	($BF),A
+	LD	A,$C0
+	OUT	($BF),A
+	
+LOC_1A9:
+	LD	A,(HL)
+	OUT	($BE),A
+	INC	HL
+	LD	A,(HL)
+	OUT	($BE),A
+	INC	HL
+	DJNZ	LOC_1A9
+	RET
+	
+LOC_1B4:
+	CP	1
+	JR	Z,LOC_1D7
+	CP	2
+	JR	Z,LOC_1D0
+	DEC	A
+	LD	($D241),A
+	IN	A,($7E)
+	LD	C,A
+	LD	A,($D242)
+	SUB	C
+	OUT	($BF),A
+	LD	A,$8A
+	OUT	($BF),A
+	JP	LOC_210
+	
+LOC_1D0:
+	DEC	A
+	LD	($D241),A
+	JP	LOC_210
+	
+LOC_1D7:
+	DEC	A
+	LD	($D241),A
+	LD	A,0
+	OUT	($BF),A
+	LD	A,$C0
+	OUT	($BF),A
+	LD	B,$10
+	LD	HL,DATA_216
+	BIT	4,(IY+7)
+	JR	Z,LOC_1F1
+	LD	HL,DATA_256
+	
+LOC_1F1:
+	LD	A,(HL)
+	OUT	($BE),A
+	INC	HL
+	NOP
+	LD	A,(HL)
+	OUT	($BE),A
+	INC	HL
+	NOP
+	LD	A,(HL)
+	OUT	($BE),A
+	INC	HL
+	LD	A,(HL)
+	INC	HL
+	OUT	($BE),A
+	DJNZ	LOC_1F1
+	LD	A,($D219)
+	AND	$EF
+	OUT	($BF),A
+	LD	A,$80
+	OUT	($BF),A
+	
+LOC_210:
+	POP	BC
+	POP	DE
+	POP	HL
+	POP	AF
+	EI
+	RET
+	
+DATA_216:
+	INCBIN	"levels/lz/palettes/underwaterA.bin"
+DATA_256:
+	INCBIN	"levels/lz/palettes/underwaterB.bin"
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Sonic The Hedgehog Game Gear Disassembly
 
-This project aims to rebuild Sonic The Hedgehog for the SEGA Game Gear system as accurately as possible while being easy to use.
+This project aims to rebuild Sonic The Hedgehog for the SEGA Game Gear system as accurately as possible while being easy to use. Inspired by TheRavenfreak's disassembly, but aiming to have more research done.
 
 ## Status
 <p align="center">
